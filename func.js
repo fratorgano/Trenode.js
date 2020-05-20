255efe33c4cc69f93fba1a0cde0ad0ff9b584250
fratorgano <francesco.torgano@outlook.it>
2020-05-20 18:06:51 +0200

    Trenode 2.0

--- func.js
+++ func.js
@@ -1,61 +1,26 @@
-var Request=require("request")
-var id=-1;
+const axios = require('axios')
+
 module.exports={
-	/* NON piu usata
-	Stazione: function Stazione(text){
-		return new Promise(function(resolve,reject){
-			var url="http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/cercaStazione/";
-		url+=text
-		//console.log("url:"+url)
-		Request.get(url, (error, response, body) => {
-		if(error) {
-			reject(error);
+	APIRequest: async function APIRequest(type, parameters){
+		switch(type){
+			case 1: url = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/"; 
+				break;
+			case 2: url = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/cercaNumeroTrenoTrenoAutocomplete/"
+				break;
+			case 3: url = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/";
+				break;
+			case 4: url = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/";
+				break;
 		}
-		resolve(body);
-		//console.log(JSON.parse(body));
-		})
-	})*/
-	Soluzione:function Soluzione(text){
-		return new Promise(function(resolve,reject){
-		var url="http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/"+text;
-		console.log("url:"+url)
-		Request.get(url, (error, response, body) => {
-		if(error) {
-			reject(error);
+		url += parameters;
+		console.log("Requested URL: "+url)
+		try {
+			const response = await axios.get(url);
+			return response.data;
+		  } catch (error) {
+			console.error(error);
 		}
-		//console.log(body)
-		resolve(body);
-		//console.log(JSON.parse(body));
-		})
-		});
-	},
-	Partenza: function Partenza(text){
-		return new Promise(function(resolve,reject){
-		var url="http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/cercaNumeroTrenoTrenoAutocomplete/"+text;
-		console.log("url:"+url)
-		Request.get(url, (error, response, body) => {
-		if(error) {
-			reject(error);
-		}
-		//console.log(body)
-		resolve(body);
-		//console.log(JSON.parse(body));
-		})
-	})
-	},
-	Treno: function Treno(text){
-		return new Promise(function(resolve,reject){
-		var url="http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/"+text;
-		console.log("url:"+url)
-		Request.get(url, (error, response, body) => {
-		if(error) {
-			reject(error);
-		}
-		//console.log(body)
-		resolve(body);
-		//console.log(JSON.parse(body));
-		})
-	})
+
 	},
 	Epoch: function Epoch(text){
 			var dateVal="/Date("+text+")/";
--- trenodeServer.js
+++ trenodeServer.js
@@ -1,95 +1,145 @@
-var compression = require('compression');
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var func = require('./func.js')
-var favicon = require('serve-favicon');
-var path = require('path');
+const compression = require('compression');
+const express = require('express');
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const path = require('path');
 
-var id=0;
-var oggetti=new Array;
+const func = require('./func.js')
+
+const app = express();
+
+const limitMemory = 10;
+var id = 0;
+const cacheSoluzioni = [];
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
 
 // app.use(express.static('public'));
 app.use(compression());
-app.use('/public', express.static(__dirname +'/public'));
-app.set("view engine", "ejs"); 
+app.use('/public', express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')));
 
-app.get("/", (req, res) => { 
-   res.render("index");
- }); 
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
-app.post('/process_post', urlencodedParser, function (req, res) {
-   response = {
-	  partenza:	req.body.cod_partenza.substr(1), // Rimuovo la prima "S"
-	  arrivo:	req.body.cod_arrivo.substr(1), 	 // Rimuovo la prima "S"
-	  data:		req.body.data,
-	  ora:		req.body.ora
-   };
-   /* var inizio=response.partenza.indexOf("(")+2
-   var fine=response.partenza.indexOf(")")
-   response.partenza=response.partenza.slice(inizio,fine)
-   var inizio=response.arrivo.indexOf("(")+2
-   var fine=response.arrivo.indexOf(")")
-   response.arrivo=response.arrivo.slice(inizio,fine) */
-   response.data+='T'
-   response.ora+=":00"
-   response.data+=response.ora
-   var promSoluzione=func.Soluzione(response.partenza+'/'+response.arrivo+'/'+response.data)
-   promSoluzione.then(
-	   function(data){
-		   soluzioni=JSON.parse(data);
-		   soluzioni.errore=String(id);
-		   //console.log(soluzioni.errore)
-		   SaveData(soluzioni);
-		   res.render('soluzioni',soluzioni);
-		},
-	   function(error){
-		   console.log(error.message);
-		   res.render('errore');
-	   });
-})
-app.post('/treno',urlencodedParser,function(req,res){
-	response={
-		nsol:req.body.nsol,
-		id:req.body.id
-	};
-	console.log("id usato array: "+response.id);
-	var ntreno=oggetti[response.id].soluzioni[response.nsol].vehicles[0].numeroTreno;
-	var promPartenza=func.Partenza(ntreno)
-   	promPartenza.then(
-	   function(data){
-		   partenza=data;
-		   inizio=data.indexOf("-",7)+1;
-		   stazionePartenza=data.slice(inizio,inizio+6);
-		   //console.log("QUi "+stazionePartenza+"qui");
-		   var promTreno=func.Treno(stazionePartenza+"/"+ntreno);
-  		   promTreno.then(
-	   		function(data){
-		   treno=JSON.parse(data);
-		   //console.log(treno);
-		   res.render('treno',treno)
-		},
-	   function(error){
-		   console.log(error.message);
-	   });
-		},
-	   function(error){
-		   console.log(error.message);
-	   });
+app.post('/process_post', urlencodedParser, async function (req, res) {
+    const response = {
+        partenza: req.body.cod_partenza.substr(1), // Rimuovo la prima "S"
+        arrivo: req.body.cod_arrivo.substr(1), // Rimuovo la prima "S"
+        data: req.body.data,
+        ora: req.body.ora
+    };
+
+    response.data += 'T'
+    response.ora += ":00"
+    response.data += response.ora
+    const soluzioniData = await func.APIRequest(1,response.partenza + '/' + response.arrivo + '/' + response.data);
+    //Debug thingy
+    soluzioniData.errore = String(id);
+
+    CachingSoluzioni(soluzioniData);
+
+    res.render('soluzioni', soluzioniData);
+
 })
 
-function SaveData(data){
-	oggetti[id]=data;
-	id++;
-	console.log(oggetti.length);
+app.post('/treno', urlencodedParser, async function (req, res) {
+    const response = {
+        nsol: req.body.nsol,
+        id: req.body.id
+    };
+    console.log("id usato array: " + response.id);
+    //console.log(soluzioni)
+
+    const tr = [];
+
+     var prom = new Promise((resolve, reject) => {
+        if(cacheSoluzioni[response.id%limitMemory] === undefined){
+            reject("failed to retrieve data")
+        }
+        let i = 0;
+        cacheSoluzioni[response.id].soluzioni[response.nsol].vehicles.forEach(async (vehicle,order) => {
+            const ntreno = vehicle.numeroTreno;
+            //console.log(ntreno, typeof ntreno);
+            if(ntreno!='Urb'){
+                let data = await func.APIRequest(2,ntreno);
+                const limite = data.indexOf("\n");
+                data = data.slice(0,limite);
+                const inizio = data.lastIndexOf("-") + 1;
+                const stazionePartenza = data.slice(inizio, inizio + 6);
+                //console.log("QUi "+stazionePartenza+"qui");
+                const train = await func.APIRequest(3, stazionePartenza + "/" + ntreno)
+                train.salita = vehicle.origine
+                train.discesa = vehicle.destinazione
+                //console.log(data2.numeroTreno);
+                tr[order] = train;
+            }else{
+                const urb = {
+                    compNumeroTreno: "Tragitto Urbano",
+                    fermate: [{
+                        stazione: vehicle.origine,
+                        id: await func.APIRequest(4,vehicle.origine)
+                    },
+                    {
+                        stazione: vehicle.destinazione,
+                        id: await func.APIRequest(4,vehicle.destinazione)
+                    }],
+                    salita: vehicle.origine,
+                    discesa: vehicle.destinazione
+                }
+                tr[order] = urb;
+            }
+            if (i === cacheSoluzioni[response.id].soluzioni[response.nsol].vehicles.length-1) resolve();
+            i++;
+        });
+    });
+    
+
+    prom.then(()=>{
+        //console.log(tr);
+
+        //WIP, test per eliminare stazioni che non devo percorrere.
+        /* for (let index = 0; index < tr.length-1; index++) {
+            let limite = -1;
+            for (let jndex = 1; jndex < tr[index].fermate.length; jndex++){
+                const fermata = tr[index].fermate[jndex];
+                //console.log(fermata.stazione +"=="+tr[index+1].fermate[0].stazione.toUpperCase());
+                // if(fermata.stazione == tr[index+1].fermate[0].stazione.toUpperCase()){ 
+                if(fermata.id == tr[index+1].fermate[0].id){
+                    limite = jndex;
+                    //console.log("SUCCESSO: "+limite);
+                    break;
+                }
+            }
+            
+            //console.log(tr[index].fermate);
+            //console.log(limite, tr[index+1].fermate[0].stazione.toUpperCase());
+            if(limite!=-1){
+                tr[index].fermate = tr[index].fermate.slice(0,limite+1);
+                //console.log("Sliced index: " + index + " to: " + (limite+1));
+            }
+        } */
+        res.render('treno', {
+            treni: tr
+        });
+    });
+});
+
+function CachingSoluzioni(soluzioni) {
+    cacheSoluzioni[id%limitMemory] = soluzioni;
+    id++;
+    console.log("Dimensione cache soluzioni: " + cacheSoluzioni.length);
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 }
 
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Treni app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Treni app listening at http://%s:%s", host, port)
 });
